72ebf3f3d978e40b332ffefbaea1cc7ddab295a0
Fabio Amorelli <amorelli.ff@gmail.com>
2021-10-21 11:37:17 -0300

    adjust html head

--- src/components/EnrollButton.svelte
+++ src/components/EnrollButton.svelte
@@ -5,7 +5,8 @@
 </script>
 
 <button
-    on:click={() => animateScroll.scrollTo({ element: '.tabs' })}
+    on:click={() =>
+        animateScroll.scrollTo({ element: '#subscriptionForm', offset: -100 })}
     class="button-enroll"
 >
     <img src="assets/img/down-arrow.png" alt="Seta apontando para baixo" />
@@ -24,6 +25,7 @@
         border: none;
         color: var(--clr-foreground-secondary);
         margin: 1rem 0;
+        cursor: pointer;
     }
 
     .button-enroll:focus {
---
+++ src/components/Form.svelte
@@ -0,0 +1,36 @@
+<script>
+    let outerWidth;
+    let iframeWidth = 768;
+</script>
+
+<svelte:window bind:outerWidth />
+
+<div class="container">
+    <iframe
+        id="subscriptionForm"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScFOAmQZc8YvdwNBydNqz0xmqyctL9NhzP3nUNByLzhHXE1Rg/viewform?embedded=true"
+        width={outerWidth < iframeWidth + 24 ? outerWidth - 24 : iframeWidth}
+        height="1550"
+        frameborder="0"
+        onload="resizeIframe(this)"
+        marginheight="0"
+        marginwidth="0">Loading…</iframe
+    >
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        width: 100%;
+        height: auto;
+        margin: 2rem 0;
+    }
+
+    iframe {
+        background-color: white;
+        border-radius: 0.5rem;
+    }
+</style>
